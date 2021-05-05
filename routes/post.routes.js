8b1a9422513d3ed1dca207afb7668112967d44e5
Matheus Bilobrovec <matheusbilobrovec@gmail.com>
2021-05-05 19:52:26 -0300

    Routa para slug

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -5,6 +5,7 @@ routes.post('/posts', postController.create)
 routes.get('/posts', postController.index)
 routes.get('/posts/count', postController.count)
 routes.get('/posts/:id', postController.get)
+routes.get('/posts/:slug', postController.get)
 routes.post('/posts/:id', postController.update)
 routes.delete('/posts/:id', postController.delete)
 
